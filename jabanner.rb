d9943b1d67a2248c3b1fc75b0cae361b301ff19b
TAKEI Yuya <takei-yuya@users.noreply.github.com>
2017-08-25 00:39:21 +0900

    update sha1 to sha256

--- jabanner.rb
+++ jabanner.rb
@@ -3,7 +3,7 @@ require "formula"
 class Jabanner < Formula
   homepage "https://github.com/takei-yuya/jabanner"
   url "https://github.com/takei-yuya/jabanner/releases/download/v0.1.1/jabanner-0.1.1.tar.gz"
-  sha1 "916a2c2c884ea0ed153e7a6658d0fcf46ac7a7d5"
+  sha256 "f64fcc41b09e283fd800c5123b3924a061bb9f275e3c5172c90f1620fe55d3c8"
 
   depends_on 'gd' => :build
 
